36a00b577478e8bd761de23c3f9f7b4524c9df24
Moreno <atunmor@gmail.com>
2024-07-03 11:52:10 -0600

    Ultimos cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Employee } from './models/employee';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/models/employee.ts
+++ src/app/models/employee.ts
@@ -1,5 +1,5 @@
 export class Employee {
-  id: number;
-  name: string;
-  country: string;
+    id: number = 0;
+    name: string = "";
+    country: string = "";
 }
